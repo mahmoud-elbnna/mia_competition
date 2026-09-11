3a85aeb60a394e42069ec1178cbd26de1c1abe98
Mahmoud Elbnna <mahmoudalaa2022o8@gmail.com>
2026-09-11 14:53:23 +0300

    update setup.py
    Updated package description, license, and test requirements in setup.py.

--- src/mia_competition/setup.py
+++ src/mia_competition/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'mia_competition'
@@ -10,20 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='melbnna',
     maintainer_email='mahmoudalaa2022o8@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    description='Competition Package',
+    license='Apache-2.0',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'auto_node = mia_competition.autonmous_phase:main',
         ],
     },
 )
